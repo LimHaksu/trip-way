069f3c857125deaaab3520e5baac8873552f274c
Lim Haksu <neroren2@gmail.com>
2020-03-18 20:40:26 +0900

    change from js to ts

---
+++ containers/Main.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+    
+}
+interface State {
+    
+}
+
+export default class Main extends Component<Props, State> {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                메인페이지
+            </div>
+        )
+    }
+}
