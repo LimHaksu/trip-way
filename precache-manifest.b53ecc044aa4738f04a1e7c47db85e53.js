b94bb8d12cf80d840ec594369a4dfdf070616e98
Lim Haksu <neroren2@gmail.com>
2020-04-15 23:09:09 +0900

    Updates

---
+++ precache-manifest.b53ecc044aa4738f04a1e7c47db85e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e830cf89eeffb751fe39492b5310ba",
+    "url": "/trip-way/index.html"
+  },
+  {
+    "revision": "5cac052ecf592f7455d6",
+    "url": "/trip-way/static/css/main.1cecc2ed.chunk.css"
+  },
+  {
+    "revision": "a0e5cb78de6f055da801",
+    "url": "/trip-way/static/js/2.e1d85c1e.chunk.js"
+  },
+  {
+    "revision": "0b9793cf4eb1bef8fddaa3658d3627d7",
+    "url": "/trip-way/static/js/2.e1d85c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cac052ecf592f7455d6",
+    "url": "/trip-way/static/js/main.7556e6ae.chunk.js"
+  },
+  {
+    "revision": "2580a8536d58bf94a992",
+    "url": "/trip-way/static/js/runtime-main.6e803641.js"
+  }
+]);
